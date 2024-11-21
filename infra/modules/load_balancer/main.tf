5606b43a19cd88fc0da6ad1f938efa5a8857fd25
Ikobi Lynch <ishaashi16@gmail.com>
2024-11-20 20:24:30 -0500

    Added load balancer listener

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -35,6 +35,17 @@ resource "aws_lb_target_group_attachment" "app_attachment" {
 
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
 output "dns_name" {
   value = aws_lb.main.dns_name
 }
